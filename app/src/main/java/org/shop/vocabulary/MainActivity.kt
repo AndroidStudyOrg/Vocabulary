d422e8bb27354b1a34b929b04f01b8d4f603a8fd
wbham <wbham97@naver.com>
2023-10-10 06:00:58 +0900

    initialsetting

---
+++ app/src/main/java/org/shop/vocabulary/MainActivity.kt
@@ -0,0 +1,16 @@
+package org.shop.vocabulary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.shop.vocabulary.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+    }
+}
